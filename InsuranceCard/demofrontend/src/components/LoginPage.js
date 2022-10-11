b1e63374fb4405f280e95ef915ba1660de990b36
PhongPham1110 <phongvupham1111@gmail.com>
2022-10-11 22:41:18 +0700

    Login page (not connected to AccountList)

--- InsuranceCard/demofrontend/src/components/LoginPage.js
+++ InsuranceCard/demofrontend/src/components/LoginPage.js
@@ -1,16 +1,60 @@
+import React, { useEffect, useState, } from "react"
+import '../styles/login.scss'
+
 const LoginPage = () => {
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [accountList, setAccountList] = useState([]);
+
+  //get data(chua lam)
+
+
+
+  const handleChangeUserName = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+  const handleLogin = () => {
+    let check = false;
+    accountList.map((account) =>
+      {return (account.username === username && account.password === password)}
+    )
+    if (check === true) {
+      //do something
+    } else {
+      alert("Login fail")
+      setUsername('')
+      setPassword('')
+    }
+  }
+
+
+
   return (
     <div>
       <div className="Login">
         <h1>Login</h1>
         <p>
-          Username <input type="text" placeholder="Enter username" />
+          Username <input
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(event) => handleChangeUserName(event)}
+          />
         </p>
         <p>
-          Password <input type="password" placeholder="Enter password" />
+          Password <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(event) => handleChangePassword(event)}
+          />
         </p>
-        <button>Login</button>
-        <button>Register</button>
+        <button onClick={() => handleLogin()}>Login</button>
       </div>
     </div>
   );
