b06a006458ebc3162d7fda8691570c55485ea39c
PhongPham1110 <phongvupham1111@gmail.com>
2022-10-18 22:46:12 +0700

    navBar full HD ko che

--- InsuranceCard/demofrontend/src/components/Nav.js
+++ InsuranceCard/demofrontend/src/components/Nav.js
@@ -12,15 +12,22 @@ const Nav = () => {
         <React.Fragment>
             <div className='topBar'>
                 <div className="topnav">
-                    <button className='sidebar-toggler' onClick={handleSidebar}><FAicons.FaBars /></button>
                     <button className='nav-link'>Home</button>
                     <button className='nav-link'>News</button>
                     <button className='nav-link'>Contact</button>
                     <button className='nav-link'>About</button>
                 </div>
-
-                <div className='profile'>
-                    
+                <div className='user-log-nav'>
+                    <button className='profile-nav lower-level-profile-nav'>User
+                        <ul className="dropdown">
+                            <li className="dropdown-item">caigido</li>
+                            <li className="dropdown-item">caigido2</li>
+                            <li className="dropdown-item">caigido3</li>
+                        </ul>
+                    </button>
+                    <button className='profile-nav'>Log (in/out)</button>
+                    <div className='profile'>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
